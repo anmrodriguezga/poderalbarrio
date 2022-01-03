9227764970ab58d600d22c0fed9beb185d9d0f25
anmrodriguezga <anmrodriguezga@unal.edu.co>
2022-01-03 16:45:16 -0500

    Fixed responsiveness in projects and publications.

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,14 +1,83 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Image, Container, Button, ButtonGroup, Dropdown } from 'react-bootstrap';
 import '../main-page/main-page.css'
 
 const Projects = () => {
     return (
-        <>
-            <Row className='g-0 fullHeight'>
-            <Col>Proyectos</Col>
-            </Row>
-        </>
+        <Row className='g-0 justify-content-center'
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+        >
+            <Col
+                xs={{span: 8, order: 'last'}}
+                sm={{span: 8, order: 'last'}}
+                md={{span: 6, order: 'first'}}
+                lg={{span: 6, order: 'first'}}
+                xl={{span: 6, order: 'first'}}
+            >
+                <Image
+                    className='card-left'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+                <Image
+                    className='card-left'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+                <Image
+                    className='card-left'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+            </Col>
+            <Col
+                xs={{span: 8, order: 'first'}}
+                sm={{span: 8, order: 'first'}}
+                md={{span: 6, order: 'last'}}
+                lg={{span: 6, order: 'last'}}
+                xl={{span: 6, order: 'last'}}
+            >
+                <Container
+                    className='banner-text'
+                    style={{ paddingTop: '90px', marginLeft: '20px', paddingRight: '10vw' }}
+                >
+                    <div className='banner-text-title'>Proyectos</div>
+                    <p><b>Creación desde el diseño y lo audiovisual,
+                        programas de salud, formación, mapeo y más.</b></p>
+                    <p>Aquí encontrarás proyectos relacionados al
+                        fortalecimiento y la participación barrial
+                        en Bogotá, consulta y comparte cada uno de
+                        estos proyectos. A continuación, puedes escoger
+                        una o varias de las siguientes categorías para
+                        filtrar los proyectos:</p>
+                    <Dropdown as={ButtonGroup}>
+                        <Button variant="success">Split Button</Button>
+
+                        <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Container>
+                <Image
+                    className='card-right'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+                <Image
+                    className='card-right'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+            </Col>
+        </Row>
     );
 }
 
--- src/publications/publications.js
+++ src/publications/publications.js
@@ -1,14 +1,87 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Image, Container, Button, ButtonGroup, Dropdown } from 'react-bootstrap';
 import '../main-page/main-page.css'
 
 const Publications = () => {
     return (
-        <>
-            <Row className='g-0 fullHeight'>
-                <Col>Publicaciones</Col>
-            </Row>
-        </>
+        <Row className='g-0 justify-content-center'
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+        >
+            <Col
+                xs={8}
+                sm={8}
+                md={6}
+                lg={6}
+                xl={6}
+            >
+                <Container
+                    className='banner-text'
+                    style={{ paddingTop: '90px', paddingRight: '20px', paddingLeft: '10vw', textAlign: 'right' }}
+                >
+                    <div className='banner-text-title-inverted'>Publicaciones</div>
+                    <p><b>Cartillas, artículos y compilados de infografías 
+                        resultado de los proyectos de la organización.</b></p>
+                    <p>En esta sección encontrarás publicaciones relacionadas 
+                        a temas como trabajo informal, derecho y salud laboral, 
+                        espacio público y barrial. Te invitamos a leer y compartir 
+                        cada uno de los recursos que aquí consignamos.</p>
+                    <Dropdown as={ButtonGroup}>
+                        <Button variant="success">Split Button</Button>
+
+                        <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Container>
+                <Image
+                    className='book-card-left'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+                <Image
+                    className='book-card-left'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+            </Col>
+            <Col
+                xs={8}
+                sm={8}
+                md={6}
+                lg={6}
+                xl={6}
+                style={{zIndex: -1}}
+            >
+                <Image
+                    className='book-card-right'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+                <Image
+                    className='book-card-right'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+                <Image
+                    className='book-card-right'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+                <Image
+                    className='book-card-right'
+                    alt='img'
+                    src='/assets/thumbnail.jpg'
+                />
+            </Col>
+        </Row>
     );
 }
 
